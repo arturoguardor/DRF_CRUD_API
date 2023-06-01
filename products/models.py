af27bd9b62834a9bf4a2335f79cab489db16a933
Arturo Guardo R <arturoguardor@gmail.com>
2023-06-01 07:13:11 +0200

    Creation models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    grade = models.DecimalField(max_digits=1, decimal_places=1)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
