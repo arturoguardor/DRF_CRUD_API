a6964973d5015a68781b3fdd8aa305b2b5b5211c
Arturo Guardo R <arturoguardor@gmail.com>
2023-06-01 07:51:57 +0200

    Config admin page

--- products/models.py
+++ products/models.py
@@ -7,9 +7,17 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return self.title
+
 
 class Review(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     grade = models.DecimalField(max_digits=1, decimal_places=1)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(
+        Product, on_delete=models.CASCADE, default=0)
+
+    def __str__(self):
+        return self.title
